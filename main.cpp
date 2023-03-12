0c82ace5c4fdc12ecdcc944f755096e48bf16d0b
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-12 16:33:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
+    cout << "============================================" << endl;
+    cout << " Quine-McCluskey Logic Minimization Program" << endl;
+    cout << "============================================" << endl;
+    int q,c;
+    do
+    {
+    cout << "How can I help you?" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "1- Perform a Quine-McCluskey operation" << endl;
+    cout << "2- Exit" << endl;
+    cin >> q;
+    c = q;
+    switch (q)
+        {
+            case 1:
+                cout << "--------------------------------------------" << endl;
+                break;
+                
+            case 2:
+                cout << "--------------------------------------------" << endl;
+                cout << "Thanks for using our Program" << endl;
+                cout << "Created by Hussein Heggi and Youssef Elmahdy" << endl;
+                exit(EXIT_SUCCESS);
+                break;
+                
+            default:
+                cout << "--------------------------------------------" << endl;
+                cout << "Error Wrong Input, please try again" << endl;
+                cout << "--------------------------------------------" << endl;
+                break;
+        }
+    }while ((q != 1) && (c != 2));
     
 }
