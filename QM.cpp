0d8b5d096694bc9cc2b0f369878bfd9c180f6e25
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-13 17:05:44 +0200

    Update QM.cpp

--- QM.cpp
+++ QM.cpp
@@ -1,5 +1,4 @@
 
-
 #include "QM.hpp"
 QM::QM()
 {
@@ -9,65 +8,60 @@ void QM::Start()
 {
     cout << "=====================================" << endl;
     ifstream test1("/Users/s7s/Desktop/Uni Spring 2023/Digital Design/DD1 Project 1/Test_File.txt");
-    test1 >> BoFun;
-    cout << Validate_Bool(BoFun) << endl;
-    cout << "Number of variables:" << num << endl;
-}
-int QM::Compare(string X, string Y)
-{
-    int i = 0;
-    while (X[i] && Y[i])
+    if (test1.fail())
     {
-        if (X[i] != Y[i])
-            return 0;
+        cout << "Error loading file" << endl;
+        exit(EXIT_SUCCESS);
+    }
+    
+    stringstream buffer;
+    buffer << test1.rdbuf();
+    BoFun = buffer.str();
+    Bool_Fun(BoFun);
+    
+    if (Validate_Bool() == false)
+    {
+        cout << "The function is not in a correct SoP form" << endl;
+        return;
+    }
         
- 
-        i++;
-    }
- 
-    return (Y[i] == '\0');
-}
- 
+    for (int i = 0; i < Variables.size(); i++)
+        cout << Variables[i] << " ";
+    cout << endl;
+    cout << "Number of variables:" << num << endl;
 
-bool QM::strstr(string X, string Y)
-{
-    int i = 0;
-    while (X[i] != '\0')
-    {
-        if ((X[i] == Y[i]) && Compare(X, Y)) {
-            return 1;
-        }
-        i++;
-    }
- 
-    return 0;
 }
-bool QM::Validate_Bool(string b)
+void QM::Bool_Fun(string b)
 {
-    Validated.clear();
     b.erase(remove_if(b.begin(), b.end(), ::isspace),b.end());
-    string stop = "+";
-    string copy = b;
-    copy.erase(remove(copy.begin(), copy.end(), '\''), copy.end());
-    size_t pos = copy.find (stop);
     string sub;
-    sub = copy.substr(0, pos);
-    num = sub.length();
-    pos = 0;
-    while (( pos = b.find (stop)) != std::string::npos)
+    stringstream X(b);
+    while (getline(X, sub, '+'))
     {
-        sub = b.substr(0, pos);
-        cout << sub <<endl;
-        Validated.push_back(sub);
-        b.erase(0, pos + stop.length());
+        Variables.push_back(sub);
     }
-//    for (int i = 0; i < num; i++)
-//        cout <<Validated[i];
-   
-    for (int i = 0; i<Validated.size(); i++)
-        for(int j = 1; j<= 1; j++)
-            if ((strstr(Validated[i], Pos_Var[j])) == 0)
+    
+}
+bool QM::Validate_Bool()
+{
+    int z = 1;
+    Validator = Variables;
+    for (int i = 0; i < Validator.size(); i++)
+    {
+        Validator[i].erase(remove(Validator[i].begin(), Validator[i].end(), '\''), Validator[i].end());
+        if (num < Validator[i].length())
+            num = Validator[i].length();
+    }
+        
+    for (int i = 0; i<Variables.size(); i++)
+        for(int j = 1; j<= num; j++)
+        {
+            if (Variables[i].find(Pos_Var[j]) != std::string::npos)
+                z = 1;
+            else
                 return false;
+        }
+            
     
     return true;
 }
