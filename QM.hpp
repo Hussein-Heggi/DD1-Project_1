a3003c1c76f98c402dbb5e7bc0e349f96c38350a
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-13 03:01:52 +0200

    Update QM.hpp

--- QM.hpp
+++ QM.hpp
@@ -4,7 +4,28 @@
 
 #include <stdio.h>
 #include <iostream>
-#include "Help_Functions.hpp"
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
 using namespace std;
+
+class QM
+{
+private:
+    long int num;
+    string Pos_Var[11] = {"'","a","b","c","d","e","f","g","h","i","j"};
+    string BoFun;
+    vector<string> Validated;
+public:
+    QM();
+    void Start();
+    void Num_Var(int);
+    bool Validate_Bool(string);
+    int Compare(string, string);
+    bool strstr(string, string);
+};
 
 #endif 
