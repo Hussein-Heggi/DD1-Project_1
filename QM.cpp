d3aefd3d200c8f94cd3d6b8f52aa9a338fcbb611
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-15 00:06:02 +0200

    Update QM.cpp

--- QM.cpp
+++ QM.cpp
@@ -1,15 +1,18 @@
 
 #include "QM.hpp"
-QM::QM(string x)
+QM::QM(string x, int n)
 {
     cout << "=============================================" << endl;
     BoFun = x;
+    num = n;
     Get_Fun(BoFun);
     Validate_Fun();
     Convert();
     
-
-
+    for (int i = 0; i < Bsterms.size(); i++)
+    {
+        cout << Bsterms[i] << " - ";
+    }
 }
 
 void QM::Get_Fun(string b)
@@ -26,20 +29,42 @@ void QM::Get_Fun(string b)
 void QM::Validate_Fun()
 {
     bool repeat;
-    Validator = Variables;
-    for (int i = 0; i < Validator.size(); i++)
+    int y;
+    for (int i = 1; i <= num ; i++)
     {
-        Validator[i].erase(remove(Validator[i].begin(), Validator[i].end(), '\''), Validator[i].end());
-        if (num < Validator[i].length())
-            num = Validator[i].length();
+        Perfect.push_back(Pos_Var[i]);
     }
         
     for (int i = 0; i<Variables.size(); i++)
+    {
+        Validator = Variables;
+        for (int i = 0; i < Validator.size(); i++)
+        {
+            Validator[i].erase(remove(Validator[i].begin(), Validator[i].end(), '\''), Validator[i].end());
+        }
         for(int j = 1; j<= num; j++)
         {
-            if (!(Variables[i].find(Pos_Var[j]) != std::string::npos))
+            if(Validator[i].length() != Perfect.length())
             {
-                
+                string Incomplete = Variables[i];;
+                string I1 = Incomplete, I2 = Incomplete;
+                y = 0;
+                for (int z = 0; z < num+y ; z++)
+                {
+                    if (Incomplete[z+y] != '\'')
+                    {
+                        if (Incomplete[z+y] != Perfect[z])
+                        {
+                            I1.insert(z+y, Add[z]);
+                            I2.insert(z+y, Bar[z]);
+                            Variables.push_back(I1);
+                            Variables.push_back(I2);
+                        }
+                    }
+                    else
+                        y++;
+                }
+                    
             }
             else
             {
@@ -62,6 +87,8 @@ void QM::Validate_Fun()
             }
                 
         }
+    }
+        
             
 }
 
@@ -85,10 +112,11 @@ void QM::Convert()
                     result.append(one);
             }
         }
-        Binterms.push_back(result);
+        Bsterms.push_back(result);
     }
     
     
 }
+
 
 
