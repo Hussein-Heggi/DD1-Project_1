1dd91ba894e131ccbb3b29278aa8e5b67905f02b
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-17 05:25:39 +0200

    Update QM.hpp

--- QM.hpp
+++ QM.hpp
@@ -23,7 +23,7 @@ private:
     string BoFun;
     string Perfect = "";
     vector<string> Variables;
-    vector<string> Validator;
+    vector<string> Validator; // Variables without the '
     vector<string> minterms; // alphabetical values of minterms
     vector<string> Bsterms; // Binary values of minterms
     vector<string> Maxterms; // Binary values of maxtrems
@@ -34,20 +34,22 @@ private:
     
     
 public:
-    QM(string, int);
-    void Get_Fun(string);
-    void Gen_Fun();
-    bool Validate_Fun(int);
-    void Convert();
+    QM(string, int); // Constructor
+    void Get_Fun(string); // removes any unnecassary chars
+    void Gen_Fun(); // Add missing literals
+    bool Validate_Fun(int); // Validate the boolean function
+    void Convert(); // Generate Binterms
     int binaryToDecimal(int); // Converts from binart to decimal
-    long long int decimalToBinary(int); // converts from decimal to binary
+    long long int decimalToBinary(int); // Converts from decimal to binary
     void TruthTable(); // Generate Truth tables using minterms
-    void RConvert();
-    void SoP_PoS();
-    static int oneCount(string s); // count number of ones
+    void RConvert(); // Generate Maxterms
+    void SoP_PoS(); // Display the canonical SoP and PoS
+    void Prime_Implicants(); // Convert PIs to literals and Display
+    static int oneCount(string s); // Count number of ones
     void ImplicationTable(); // Generate Implication table and prime implicants
     bool CompareBits(string,string); // Identify complements
     string ReplaceBits(string,string); // When complementa are identified, they're replaced by _
+    void Essential_PI(); // Generate and Display EPIs
     
     
 };
