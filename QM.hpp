079c84d7efc66f5048cc223e54781bc731af0cfe
Hussein-Heggi <98839708+Hussein-Heggi@users.noreply.github.com>
2023-03-18 20:28:15 +0200

    Update QM.hpp

--- QM.hpp
+++ QM.hpp
@@ -1,5 +1,3 @@
-
-
 #ifndef QM_hpp
 #define QM_hpp
 
@@ -35,7 +33,8 @@ private:
     vector<string> PIs; // Prime Implicants
     vector<string> PreEssential; //copy of PIs with dontcares
     vector<string> EPIs; // Essential Prime Implicants
-    
+    vector<string> covered; // Covered minterms by EPIs
+    vector<string> uncovered; // complement of covered
     
 public:
     QM(string, int); // Constructor
@@ -54,8 +53,6 @@ public:
     bool CompareBits(string,string); // Identify complements
     string ReplaceBits(string,string); // When complementa are identified, they're replaced by _
     void Essential_PI(); // Generate and Display EPIs
-    
-    
 };
 
 #endif
